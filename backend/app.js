a23b8a620017cf62087eeb45532a1900a638fa43
Bobby Gupta <gupta298@purdue.edu>
2018-02-11 12:48:05 -0500

    Code to connect the backend the database

--- backend/app.js
+++ backend/app.js
@@ -43,4 +43,18 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/local';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db.close();
+});
+
 module.exports = app;
