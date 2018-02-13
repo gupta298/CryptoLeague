069b4e891e3978d6758087cf73dc02808d3cf357
Nisarg Kolhe <nisargkolhe@gmail.com>
2018-02-13 12:24:37 -0500

    Added User class

---
+++ frontend/src/app/user.ts
@@ -0,0 +1,10 @@
+export class User {
+	id: number;
+    email: string;
+    firstname: string;
+    lastname: string;
+    tokens: number;
+    profilePicture: string;
+    currentLeague: string;
+    //pastLeagues: League[]; 
+}
