134ad90de70d37681c27cca9ec5ac0fad9207a3f
Utkarsh Jain <utkjain@gmail.com>
2018-02-18 18:27:19 -0500

    NEWS API Works

--- backend/routes/newsapi.js
+++ backend/routes/newsapi.js
@@ -1,6 +1,7 @@
 
 var fetch = require('isomorphic-fetch');
 var Request = require('request');
+var request = require('request');
 var express = require('express');
 var router = express.Router();
 
@@ -11,14 +12,30 @@ var url = 'https://newsapi.org/v2/everything?q=cryptocurrency&sortBy=publishedAt
 router.get('/', function(req, res) {
  var req = new Request(url);
  //res.send(req.json());
-  fetch(req)
-      .then(function(response) {
-          if (response.ok) {
-            res.send("lol, wont print");
-          } else {
-            res.send("Error hai bhai");
-          }
-      });
+  callNewsOrgAPI();
   });
 
 module.exports = router;
+
+function callNewsOrgAPI() {
+  request({
+      url: url,
+      json: true
+  }, function (error, response, body) {
+      if (!error) {
+          var data = JSON.parse(JSON.stringify(body));
+          var tempNews = [];
+          var counter = 0;
+          while(counter < 16)
+          {
+              tempNews.push(response.body.articles[0]);
+              console.log(response.body.articles[0]);
+              counter++;
+          }
+          //console.log(response.body);
+          // console.log(JSON.stringify(coinData));
+      } else {
+        console.log("Error updating the news data");
+      }
+  });
+}
