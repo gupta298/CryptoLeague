cb22d2c490f9ce021e6b5de2c9e757d9de7ce355
Nisarg Kolhe <nisargkolhe@gmail.com>
2018-02-21 00:26:38 -0500

    All unit tests passing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,18 +4,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpModule, Http, Headers, Response, RequestOptions } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { LandingComponent } from './landing/landing.component';
 import { HomeComponent } from './home/home.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { VerifyComponent } from './verify/verify.component';
+import { MarketComponent } from './market/market.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 import { AuthGuard } from './auth.guard';
 
 import { AuthenticationService, NewsService, MarketService, UserService } from './services/index';
-import { MarketComponent } from './market/market.component';
-import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+
 
 @NgModule({
   declarations: [
