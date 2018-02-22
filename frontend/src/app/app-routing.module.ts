685b6793e9080b41731de0b06106f62a7af7af62
Nisarg Kolhe <nisargkolhe@gmail.com>
2018-02-21 20:06:34 -0500

    Added canActivate to Settings

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
 	{ path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
   	{ path: 'leaderboard', component: LeaderboardComponent, canActivate: [AuthGuard]},
 	{ path: 'market', component: MarketComponent, canActivate: [AuthGuard]},
-	{ path: 'settings', component: SettingsComponent},
+	{ path: 'settings', component: SettingsComponent, canActivate: [AuthGuard]},
     { path: '**', redirectTo: '' }
 ];
 
