71febea292ede89e1d0c16f156d02a1bdbb941af
Ritwik Bhardwaj <ritwikbhardwaj098@gmail.com>
2018-02-21 20:59:12 -0500

    validation added

--- frontend/src/app/landing/landing.component.ts
+++ frontend/src/app/landing/landing.component.ts
@@ -15,6 +15,7 @@ declare var UIkit: any;
 export class LandingComponent implements OnInit {
 
 	user: User;
+	userExists: boolean;
 
 	constructor(
 		private router: Router,
@@ -27,22 +28,34 @@ export class LandingComponent implements OnInit {
   onSubmit(form) { 
   	this.submitted = true;
   	console.log('username: ',this.user.username);
-  	this.userService.updateUser(this.user)
-      .subscribe(
-        result => {
-          console.log(result);
-          this.authService.saveJwt(result.jwt);
-          this.router.navigate(['/dashboard']);
+  	this.userService.isUsernameValid(this.user)
+  		.subscribe(
+  			result => {
+	          console.log("user exists: ",result);
+	          if(!result.exists) {
+	          	this.userService.updateUser(this.user)
+					      .subscribe(
+					        result => {
+					          console.log(result);
+					          this.authService.saveJwt(result.jwt);
+					          this.router.navigate(['/dashboard']);
 
-        }, error => {
-          console.log(error);
-        }
-    	);
-
+					        }, error => {
+					          console.log(error);
+					        }
+					    	);
+	          } else {
+	          	this.userExists = true;
+	          }
+	        }, error => {
+	          console.log(error);
+	        }
+  		)
   }
 
   ngOnInit() {
   	this.user = this.authService.loadUserFromLocalStorage();
+  	this.userExists = false;
   	//console.log(this.user);
   }
 
