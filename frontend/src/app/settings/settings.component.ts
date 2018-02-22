80cf1f444229fa7710425fa2796d537e959ffb8f
Bobby Gupta <gupta298@purdue.edu>
2018-02-21 20:16:30 -0500

    Added the setting page update request to the backend

--- frontend/src/app/settings/settings.component.ts
+++ frontend/src/app/settings/settings.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { NgForm, NgModel, NG_VALUE_ACCESSOR, FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { AuthenticationService, UserService } from '../services/index';
+import { Router } from '@angular/router';
+
+declare var UIkit: any;
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +13,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  	user: User;
 
-  ngOnInit() {
-  }
+   	constructor(
+		private router: Router,
+	    private authService: AuthenticationService,
+	    private userService: UserService,
 
+	  ) { }
+  	submitted = false;
+
+	onSubmit(form) { 
+		this.submitted = true;
+		console.log('username: ', this.user.username);
+		this.userService.updateUser(this.user)
+	  .subscribe(
+	        result => {
+	          console.log(result);
+	          this.authService.saveJwt(result.jwt);
+	        }, error => {
+	          console.log(error);
+	        }
+		);
+	}
+
+	ngOnInit() {
+		this.user = this.authService.loadUserFromLocalStorage();
+	}
 }
