9ed048adccd9257257f55ddd7402b9811ac97fe2
Utkarsh Jain <utkjain@gmail.com>
2018-03-04 16:46:04 -0500

    Added files for League select

---
+++ frontend/src/app/league-select/league-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-league-select',
+  templateUrl: './league-select.component.html',
+  styleUrls: ['./league-select.component.scss']
+})
+export class LeagueSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
