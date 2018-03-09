374ffc3a08af31259c3ed5ec29dbfac9347afd46
Nisarg Kolhe <nisargkolhe@gmail.com>
2018-03-09 15:16:04 -0500

    Added demo draggable piechart to portfolio component

--- frontend/src/app/portfolio/portfolio.component.ts
+++ frontend/src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+declare var DraggablePiechart: any;
+
 @Component({
-  selector: 'app-portfolio',
-  templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+	selector: 'app-portfolio',
+	templateUrl: './portfolio.component.html',
+	styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.setupPieChart();
+	}
 
+	setupPieChart() {
+		var proportions = [
+		{ proportion: 50, format: { color: "#2665da", label: 'Cats'}},
+		{ proportion: 50, format: { color: "#6dd020", label: 'Dogs'}} ];
+
+		var setup = {
+			canvas: document.getElementById('piechart'),
+			radius: 0.9,
+			collapsing: true,
+			proportions: proportions,
+	        //drawSegment: this.drawSegmentOutlineOnly,
+	        drawNode: this.drawNode,
+	        onchange: this.onPieChartChange
+	    };
+
+	    var newPie = new DraggablePiechart(setup);
+
+	}
+
+	drawNode (context, piechart, x, y, centerX, centerY, hover) {
+		context.save();
+		context.translate(centerX, centerY);
+		context.fillStyle = '#fefefe';
+
+		var rad = hover ? 7 : 5;
+		context.beginPath();
+		context.arc(x, y, rad, 0, Math.PI * 2, true);
+		context.fill();
+        //context.stroke();
+        context.restore();
+    }
+
+    onPieChartChange(piechart) {
+    	var table = $('#proportions-table');
+    	var percentages = piechart.getAllSliceSizePercentages();
+        //console.log("percentages", percentages);
+
+    }
 }
