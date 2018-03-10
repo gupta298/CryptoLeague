7f07b00334d51033ee7bfe00943cb4ce39374309
Utkarsh Jain <utkjain@gmail.com>
2018-03-10 18:00:49 -0500

    Added random feature

--- backend/routes/leagueTypes.js
+++ backend/routes/leagueTypes.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
-require('../config/passport');
-
 var config = require('../config/config');
+var mongo = require('../utils/mongoDBCalls');
+
+router.get('/', (req, res) => {
+    console.log("League Type: " + req.league.id);
+
+    mongo.getTotalUsers(function(error, response) {
+      res.send(response);
+    });
+  }
+);
+
+module.exports = router;
