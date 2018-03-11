41a933f65e0f1895ddfd35f7087e5686b7ea96c9
Bobby Gupta <gupta298@purdue.edu>
2018-03-10 19:10:54 -0500

    Cleaned the backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,30 +9,6 @@ require('../config/passport');
 var router = express.Router();
 const passport = require('passport');
 
-/*
-app.post("/api/login", function(req, res) {
-  if(req.body.name && req.body.password){
-    var name = req.body.name;
-    var password = req.body.password;
-  }
-  // usually this would be a database call:
-  var user = usersArr[_.findIndex(usersArr, {name: name})];
-  if( ! user ){
-    res.status(401).json({message:"no such user found"});
-  }
-
-  if(user.password === req.body.password) {
-    // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
-    var payload = {id: user.id}; //TODO: Add user object here instead
-
-    var jwtToken = token.generateAccessToken(payload);
-    res.json({message: "ok", token: jwtToken});
-  } else {
-    res.status(401).json({message:"passwords did not match"});
-  }
-});
-*/
-
 router.get('/facebook/',
   passport.authenticate('facebook', {scope: ['email', 'public_profile', 'user_photos', 'user_friends', 'user_about_me'], session: false }));
 
