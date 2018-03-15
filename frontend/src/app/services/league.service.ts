ec187fb9b92812d9231e69bd3af07ef813a4047b
Nisarg Kolhe <nisargkolhe@gmail.com>
2018-03-15 12:47:41 -0500

    Updated GET /league on league service

--- frontend/src/app/services/league.service.ts
+++ frontend/src/app/services/league.service.ts
@@ -15,12 +15,12 @@ export class LeagueService {
       return this.http.get(environment.apiUrl+'/league_types/', this.authService.generateJwt()).map((response: Response) => response.json());
   }
 
-  getLeague() {
-  	  return this.http.get(environment.apiUrl+'/league/', this.authService.generateJwt()).map((response: Response) => response.json());
+  getLeague(leagueID: string) {
+  	  return this.http.get(environment.apiUrl+'/league/'+leagueID, this.authService.generateJwt()).map((response: Response) => response.json());
   }
 
   joinLeague(leagueType: number) {
-  	  return this.http.put(environment.apiUrl+'/user', {"league_type_id": leagueType}, this.authService.generateJwt()).map((response: Response)=> response.json());
+  	  return this.http.post(environment.apiUrl+'/league/', {"league_type_id": leagueType}, this.authService.generateJwt()).map((response: Response)=> response.json());
   }
 
 }
