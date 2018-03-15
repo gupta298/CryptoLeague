b282ae1c7799588d77b80939d9092e6823b518db
Nisarg Kolhe <nisargkolhe@gmail.com>
2018-03-15 12:48:14 -0500

    Added alert and redirect on selecting a league

--- frontend/src/app/league-select/league-select.component.ts
+++ frontend/src/app/league-select/league-select.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LeagueService } from '../services/index';
+import { Router } from '@angular/router';
 
 declare var UIkit: any;
 
@@ -10,14 +11,27 @@ declare var UIkit: any;
 })
 export class LeagueSelectComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private leagueService: LeagueService,
+    private router: Router) { }
 
   ngOnInit() {
   }
 
   selected(leagueType) {
-    UIkit.modal.confirm('Please make sure that you are joining the league. Once you join, you will not be able to exit the league. ').then(function() {
-      console.log(leagueType);
+    UIkit.modal.confirm('Please make sure that you are joining the league. Once you join, you will not be able to exit the league. ').then(()=>{
+      UIkit.alert('#joiningAlert', {});
+
+      this.leagueService.joinLeague(leagueType).subscribe(
+          result => {
+            UIkit.alert('#joiningAlert').close();
+            this.router.navigate(['/league/'+result.league_id]);
+            console.log(result);
+          }, error => {
+            UIkit.alert(UIkit.alert('#joiningAlert')).close();
+            console.log(error);
+          } 
+        );
     }, function () {
       console.log('Cancel.');
     });
