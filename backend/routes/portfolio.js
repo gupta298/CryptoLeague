3b8cc1c787af0bf1179327c904425300e20f575c
Utkarsh Jain <utkjain@gmail.com>
2018-03-21 14:19:27 -0400

    Fixed Bug

--- backend/routes/portfolio.js
+++ backend/routes/portfolio.js
@@ -71,25 +71,31 @@ router.put('/', passport.authenticate(['jwt'], { session: false }), (req, res) =
 	// Follow the below logic please:
 		// check if the user is in a league
 		if (req.user.currentLeague_id) {
-			// mongo.getPortfolio(req.user.currentLeague_id, req.user._id, req.user._id, function(error, response) {
-		  //     res.send(response);
-		  //   });
+			mongo.getLeague(req.user.currentLeague_id, req.user._id, function(error, response) {
+				if (response.status.toString() === "Waiting" ||
+						response.status.toString() === "Waiting_Locked" ||
+						response.status.toString() === "Locked") {
+							res.send(response);
+						} else {
+							res.send({'message' : "League Locked"});
+						}
+			});
 		} else {
 			res.send({'message' : "Not in any league"})
 		}
 		//check the status of the league (it has to be "Waiting" or "Waiting_Locked" or "Locked" to make any changes to the portfolio)
-		if (req.user.currentLeague_id.status == "Waiting" ||
-				req.user.currentLeague_id.status == "Waiting_Locked" ||
-				req.user.currentLeague_id.status == "Locked"
-			  ) {
-			mongo.getPortfolio(req.user.currentLeague_id, req.user._id, req.user._id, function(error, response) {
-		      res.send(response);
-		    });
-		} else {
-			res.send({'message' : "Portfolio Locked"})
-		}
+		// if (req.user.currentLeague_id.status == "Waiting" ||
+		// 		req.user.currentLeague_id.status == "Waiting_Locked" ||
+		// 		req.user.currentLeague_id.status == "Locked"
+		// 	  ) {
+		// 	mongo.getPortfolio(req.user.currentLeague_id, req.user._id, req.user._id, function(error, response) {
+		//       res.send(response);
+		//     });
+		// } else {
+		// 	res.send({'message' : "Portfolio Locked"})
+		// }
 		// 3. Check to see of the portfolio is valid
-		
+
 		// 4. update the portfolio and return the final updated portfolio
 });
 
