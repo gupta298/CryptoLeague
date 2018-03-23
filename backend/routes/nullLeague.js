619b0f540fc74aa7aa11b6d4b188854b0f0f5886
Utkarsh Jain <utkjain@gmail.com>
2018-03-23 17:52:36 -0400

    Nulling works

--- backend/routes/nullLeague.js
+++ backend/routes/nullLeague.js
@@ -11,13 +11,15 @@ router.get('/', passport.authenticate(['jwt'], { session: false }), (req, res) =
   //console.log(req.user.currentLeague_id);
   if (req.user.currentLeague_id) {
 		mongo.getLeague(req.user.currentLeague_id, req.user._id, function(error, response) {
-        req.user.currentLeague_id = 'null';
-        mongo.updateUserLeague(req.user, function(error, result) {
-          if(error)
-            console.log(error);
+        if(response.status == 4) {
+          req.user.currentLeague_id = null;
+          mongo.updateUserLeague(req.user, function(error, result) {
+          if(error) console.log(error);
           console.log('User is no longer in a league');
-        });
-	      res.send(response);
+          });
+        } else {
+          res.send({'message' : "League has not ended yet"});
+        }
 	    });
 	} else {
 		res.send({'message' : "Not a league"});
