d2d94d249d9b45b1b96a311d6411f84dc283fe4b
Nisarg Kolhe <nisargkolhe@gmail.com>
2018-03-23 16:30:40 -0400

    Added test cases for leagueSelect

--- frontend/src/app/league-select/league-select.component.spec.ts
+++ frontend/src/app/league-select/league-select.component.spec.ts
@@ -30,4 +30,24 @@ describe('LeagueSelectComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show 4 league types', () => {
+    expect(fixture.nativeElement.querySelectorAll('.leagueCard').length).toEqual(4);
+  });
+
+  it('should show Bronze league', () => {
+    expect(fixture.nativeElement.querySelector('.page-content').innerText).toContain("Bronze League");
+  });
+
+  it('should show Silver league', () => {
+    expect(fixture.nativeElement.querySelector('.page-content').innerText).toContain("Silver League");
+  });
+
+  it('should show Gold league', () => {
+    expect(fixture.nativeElement.querySelector('.page-content').innerText).toContain("Gold League");
+  });
+
+  it('should show Platinum league', () => {
+    expect(fixture.nativeElement.querySelector('.page-content').innerText).toContain("Platinum League");
+  });
 });
