8cc8819e1ac47149f5e05e4a5d832c0108916253
Nisarg Kolhe <nisargkolhe@gmail.com>
2018-03-23 23:54:24 -0400

    Updated calculatePortfolio and endLeaguelogin

--- backend/utils/mongoDBCalls.js
+++ backend/utils/mongoDBCalls.js
@@ -197,13 +197,25 @@ function endLeague(league_id){
     if (err) throw err;
     var dbo = db.db("cryptoleague_database");
 
-    //@utkyboy Add code here
+    dbo.collection("Leagues").findOne({'league_id' : league_id}, function(err, result) {
+      if (err) throw err;
 
-    db.close();
+      if (result) {
+        calculatePortfoliosValues(result, function(){
+          MongoClient.connect(mongodbUrl, function (err, db) {
+            if (err) throw err;
+            var dbo = db.db("cryptoleague_database");
+            dbo.collection("Leagues").findOneAndUpdate({'league_id': result.league_id}, {$set: {'portfolio_ids' : result.portfolio_ids}});
+            db.close();
+          });
+        });
+      }
+      db.close();
+    });
   });
 }
 
-function calculatePortfoliosValues(league) {
+function calculatePortfoliosValues(league, callback) {
   // All of the validations should be done before calling this function and you have to pass the entire league object to it
 
   var current_coin_data = market.getCurrentCoinPrices();
@@ -212,31 +224,26 @@ function calculatePortfoliosValues(league) {
   asyncLoop(league.portfolio_ids, function (item, next) {
     getPortfolioWithID(item.portfolio_id, function(err, portfolio) {
       var overall_percentage_value = 0;
-      for (index in portfolio.holdings) {
-        var found_current_market = current_coin_data.find( coin => coin.symbol.toString() === portfolio.holdings[index].coin_symbol.toString() );
-        var found_league_market = league_coins.find( coin => coin.symbol.toString() === portfolio.holdings[index].coin_symbol.toString() );
+      if(portfolio && portfolio.holdings){
+        for (index in portfolio.holdings) {
+          var found_current_market = current_coin_data.find( coin => coin.symbol.toString() === portfolio.holdings[index].coin_symbol.toString() );
+          var found_league_market = league_coins.find( coin => coin.symbol.toString() === portfolio.holdings[index].coin_symbol.toString() );
 
-        var return_over_period = ((found_current_market.price - found_league_market.price) / found_league_market.price);
-        var value = portfolio.holdings[index].percentage * return_over_period;
+          var return_over_period = ((found_current_market.price - found_league_market.price) / found_league_market.price);
+          var value = portfolio.holdings[index].percentage * return_over_period;
 
-        if (portfolio.captain_coin && portfolio.captain_coin.toString() === portfolio.holdings[index].coin_symbol.toString()) {
-          value *= 2;
+          if (portfolio.captain_coin && portfolio.captain_coin.toString() === portfolio.holdings[index].coin_symbol.toString()) {
+            value *= 2;
+          }
+
+          overall_percentage_value += value;
         }
-
-        overall_percentage_value += value;
       }
 
       item.portfolio_value = overall_percentage_value;
       next();
     });
-  }, function () {
-    MongoClient.connect(mongodbUrl, function (err, db) {
-      if (err) throw err;
-      var dbo = db.db("cryptoleague_database");
-      dbo.collection("Leagues").findOneAndUpdate({'league_id': league.league_id}, {$set: {'portfolio_ids' : league.portfolio_ids}});
-      db.close();
-    });
-  });
+  }, callback);
 }
 
 module.exports = {
@@ -561,11 +568,48 @@ module.exports = {
   function getLeague(league_id, user_id, callback) {
     MongoClient.connect(mongodbUrl, function (err, db) {
       if (err) throw err;
-        var dbo = db.db("cryptoleague_database");
-        dbo.collection("Leagues").findOne({'league_id' : league_id}, function(err, result) {
-          if (err) throw err;
+      var dbo = db.db("cryptoleague_database");
 
-          if (result) {
+      dbo.collection("Leagues").findOne({'league_id' : league_id}, function(err, result) {
+        if (err) throw err;
+
+        if (result) {
+
+          if(result.status === "2"){
+            calculatePortfoliosValues(result, function(){
+              var foundUser = false;
+              asyncLoop(result.portfolio_ids, function (item, next) {
+                if (item) {
+                  if (item.user_id.toString() === user_id.toString()) {
+                    foundUser = true;
+                  } else {
+                    if (result.status.toString() !== '4') {
+                      item.portfolio_id = null;
+                    }
+                  }
+                }
+                next();
+              }, function () {
+                var response = {
+                  _id: result._id,
+                  portfolio_ids: result.portfolio_ids,
+                  league_id: result.league_id,
+                  league_type: result.league_type,
+                  status: result.status,
+                  start_time: result.start_time
+                };
+                if (foundUser == false) {
+                  if (response.status.toString() === '4') {
+                    callback(null, JSON.parse(JSON.stringify(response)));
+                  } else {
+                    callback(null, {'message' : "Access denied! User not in the league!"});
+                  }
+                } else {
+                  callback(null, JSON.parse(JSON.stringify(response)));
+                }
+              });
+            });
+          } else {
             var foundUser = false;
             asyncLoop(result.portfolio_ids, function (item, next) {
               if (item) {
@@ -579,21 +623,30 @@ module.exports = {
               }
               next();
             }, function () {
+              var response = {
+                _id: result._id,
+                portfolio_ids: result.portfolio_ids,
+                league_id: result.league_id,
+                league_type: result.league_type,
+                status: result.status,
+                start_time: result.start_time
+              };
               if (foundUser == false) {
-                if (result.status.toString() === '4') {
-                  callback(null, JSON.parse(JSON.stringify(result)));
+                if (response.status.toString() === '4') {
+                  callback(null, JSON.parse(JSON.stringify(response)));
                 } else {
                   callback(null, {'message' : "Access denied! User not in the league!"});
                 }
               } else {
-                callback(null, JSON.parse(JSON.stringify(result)));
+                callback(null, JSON.parse(JSON.stringify(response)));
               }
             });
-          } else  {
-            callback(null, { 'message' : "League does not exist!" });
           }
+        } else  {
+          callback(null, { 'message' : "League does not exist!" });
+        }
 
-          db.close();
+        db.close();
       });
     });
   },
