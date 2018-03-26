5a07c6a64e8f33801e036e26101a5ad1e3298e8a
Ritwik Bhardwaj <ritwikbhardwaj098@gmail.com>
2018-03-26 00:37:20 -0400

    test added for portfolio

--- frontend/src/app/portfolio/portfolio.component.spec.ts
+++ frontend/src/app/portfolio/portfolio.component.spec.ts
@@ -2,15 +2,18 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, Http, Headers, Response, RequestOptions } from '@angular/http';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { By } from '@angular/platform-browser';
 import { PortfolioComponent } from './portfolio.component';
 
 import { AuthenticationService, MarketService, PortfolioService } from '../services';
 import { AuthenticationServiceStub } from '../stubs/authentication.service.stub'
 
+import { Portfolio } from '../portfolio';
+
 describe('PortfolioComponent', () => {
   let component: PortfolioComponent;
   let fixture: ComponentFixture<PortfolioComponent>;
+  let debugTable: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,9 +28,19 @@ describe('PortfolioComponent', () => {
     fixture = TestBed.createComponent(PortfolioComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debugTable = fixture.debugElement.queryAll(By.css('.ng-test-portfolio'));
+    let portfolio = new Portfolio();
+    portfolio.portfolio_id = "1";
+    portfolio.holdings = [{"percentage":35, "coin_symbol": "BTC"}, {"percentage":35, "coin_symbol": "XRP"}, {"percentage":30, "coin_symbol": "ETH"}];
+    portfolio.captain_coin = "BTC";
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('check if modal is populating correctly, and with the right message', () => {
+    expect(fixture.nativeElement.querySelector('.ng-test-portfolio').innerText).toContain("validating");
+  });
+
 });
--- frontend/src/app/services/portfolio.service.spec.ts
+++ frontend/src/app/services/portfolio.service.spec.ts
@@ -4,12 +4,24 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { PortfolioService, AuthenticationService } from '../services';
 
+import { Portfolio } from '../portfolio';
+
+let testService: PortfolioService;
+let mockPaste: Portfolio;
+let responsePropertyNames, expectedPropertyNames;
+let portfolio;
 describe('PortfolioService', () => {
 	beforeEach(() => {
+		
 		TestBed.configureTestingModule({
 			imports: [ HttpModule, RouterTestingModule ], 
 			providers: [ PortfolioService, AuthenticationService ]
 		});
+
+		portfolio = new Portfolio();
+    portfolio.portfolio_id = "1";
+    portfolio.holdings = [{"percentage":35, "coin_symbol": "BTC"}, {"percentage":35, "coin_symbol": "XRP"}, {"percentage":30, "coin_symbol": "ETH"}];
+    portfolio.captain_coin = "BTC";
 	});
 
 	it('should be created', inject([PortfolioService], (service: PortfolioService) => {
