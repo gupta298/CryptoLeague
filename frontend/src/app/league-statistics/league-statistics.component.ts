24efe3ff09e2dac6f3e93d2e4426f597df13d757
Utkarsh Jain <utkjain@gmail.com>
2018-03-31 16:10:40 -0400

    updated favicon

--- frontend/src/app/league-statistics/league-statistics.component.ts
+++ frontend/src/app/league-statistics/league-statistics.component.ts
@@ -41,24 +41,24 @@ export class LeagueStatisticsComponent implements OnInit {
 
   ngOnChanges(changes: SimpleChanges) {
   	if(this.league.portfolio_ids){
-      this.buy_in = this.league.league_buy_in;
+      //this.buy_in = this.league.league_buy_in;
       console.log("chutiya");
       console.log(this.league);
-      this.totalPool = this.buy_in * length;
+      //this.totalPool = this.buy_in * length;
 	    this.length = this.league.portfolio_ids.length;
 	    // this.topTen = Math.ceil(this.length / 10);
 	    // this.topTwentyfive = Math.ceil(this.length / 4);
 	    // this.topFifty = Math.ceil(this.length / 2);
 
       //let numTop25 = 0, numTop50 = 0, numTop75 = 0;
-      for(let i = 0; i < this.league.portfolio_ids.length; i++){
-        if(this.league.portfolio_ids[i].rank <= 25)
-          this.topTwentyFive++;
-        if(this.league.portfolio_ids[i].rank <= 50)
-          this.topFifty++;
-        if(this.league.portfolio_ids[i].rank <= 75)
-          this.topSeventyFive++;
-      }
+      // for(let i = 0; i < this.league.portfolio_ids.length; i++){
+      //   if(this.league.portfolio_ids[i].rank <= 25)
+      //     this.topTwentyFive++;
+      //   if(this.league.portfolio_ids[i].rank <= 50)
+      //     this.topFifty++;
+      //   if(this.league.portfolio_ids[i].rank <= 75)
+      //     this.topSeventyFive++;
+      // }
       // this.topTwentyFive = numTop25;
       // this.topFifty = numTop50;
       // this.topSeventyFive = numTop75;
@@ -68,10 +68,10 @@ export class LeagueStatisticsComponent implements OnInit {
           this.topPool += (0.2 * this.totalPool) / this.topTwentyFive;
         }
         if(this.league.portfolio_ids[i].rank <= 50) {
-          this.middlePool += (0.3 * this.totalPool) / this.topFifty;
+        //  this.middlePool += (0.3 * this.totalPool) / this.topFifty;
         }
         if(this.league.portfolio_ids[i].rank <= 75) {
-          this.lowerPool += (0.5 * this.totalPool) / this.topSeventyFive;
+        //  this.lowerPool += (0.5 * this.totalPool) / this.topSeventyFive;
         }
       }
 
