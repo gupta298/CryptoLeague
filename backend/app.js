042922ecc3e637ba1df35d484f36eaf2fd4508ab
EC2 Default User <ec2-user@ip-172-31-34-198.us-west-2.compute.internal>
2018-03-31 04:01:01 +0000

    Added cors check

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.use(function(req, res, next) {
        res.setHeader('Access-Control-Allow-Origin', origin);
   }
   //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
   return next();
