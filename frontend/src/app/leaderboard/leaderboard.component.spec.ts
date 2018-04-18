7cc29714b63d2787741c437737b0e5fca4bd5e77
Nisarg Kolhe <nisargkolhe@gmail.com>
2018-04-18 00:56:14 -0400

    Fixed leaderboardComponent unit tests

--- frontend/src/app/leaderboard/leaderboard.component.spec.ts
+++ frontend/src/app/leaderboard/leaderboard.component.spec.ts
@@ -7,42 +7,25 @@ import { HttpModule, Http, Headers, Response, RequestOptions } from '@angular/ht
 
 import { AuthenticationService, UserService, AlertService } from '../services/index'; 
 
+import { AuthenticationServiceStub } from '../stubs/authentication.service.stub';
+import { UserServiceStub } from '../stubs/user.service.stub';
+
 import { User } from '../user';
 
 describe('LeaderboardComponent', () => {
   let component: LeaderboardComponent;
   let fixture: ComponentFixture<LeaderboardComponent>;
+  let userService: UserService;
 
-  let userServiceStub = {
-    loadUserFromLocalStorage() {
-      let currentUser = new User();
-      currentUser.firstname = "Nisarg";
-      return currentUser;
-    },
-
-    generateJwt() {
-      let jwtToken = localStorage.getItem('jwtToken');
-        if (jwtToken) {
-            let headers = new Headers({ 'Authorization': 'Bearer ' + jwtToken });
-            return new RequestOptions({ headers: headers });
-        } else {
-            return null;
-        }
-    }
-  };
-
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [ HttpModule, RouterTestingModule ],
       declarations: [ LeaderboardComponent, SidebarComponent ],
-      providers: [ {provide: AuthenticationService, useValue: userServiceStub }, UserService, AlertService ]
+      providers: [ {provide: AuthenticationService, useClass: AuthenticationServiceStub }, { provide: UserService, useClass: UserServiceStub }, AlertService ]
     })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(LeaderboardComponent);
     component = fixture.componentInstance;
+    userService = TestBed.get(UserService);
     fixture.detectChanges();
   });
 
