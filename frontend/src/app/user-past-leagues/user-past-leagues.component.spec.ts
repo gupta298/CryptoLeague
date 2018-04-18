30b3e9b6c358a61985b4e649efd5035cad8a978d
Nisarg Kolhe <nisargkolhe@gmail.com>
2018-04-18 00:49:55 -0400

    Fixed userPastLeaguesComponent unit tests

--- frontend/src/app/user-past-leagues/user-past-leagues.component.spec.ts
+++ frontend/src/app/user-past-leagues/user-past-leagues.component.spec.ts
@@ -1,25 +1,56 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { UserPastLeaguesComponent } from './user-past-leagues.component';
+
+import { User } from '../user';
 
 describe('UserPastLeaguesComponent', () => {
   let component: UserPastLeaguesComponent;
   let fixture: ComponentFixture<UserPastLeaguesComponent>;
+  let testHostComponent: TestHostComponent;
+  let testHostFixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserPastLeaguesComponent ]
+      declarations: [ UserPastLeaguesComponent, TestHostComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserPastLeaguesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    testHostFixture = TestBed.createComponent(TestHostComponent);
+    testHostComponent = testHostFixture.componentInstance;
+    let currentUser = new User();
+    currentUser.firstname = "John";
+    currentUser.lastname = "Doe";
+    currentUser.username = "johndoe";
+    currentUser.email = "johndoe@email.com";
+    currentUser.jwtToken = "";
+    currentUser.profilePicture = "";
+    currentUser.tokens = 25;
+    currentUser.currentLeague_id = null;
+    currentUser.pastLeagues = [];
+    currentUser.email_notification = false;
+    testHostComponent.user = currentUser;
+    testHostFixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(testHostComponent).toBeTruthy();
   });
+
+  @Component({
+    selector: `host-component`,
+    template: `<app-user-past-leagues
+        [user]="user"
+      ></app-user-past-leagues>`
+  })
+  class TestHostComponent {
+    user: User;
+
+    setUser(user: User) {
+      this.user = user;
+    }
+  }
 });
